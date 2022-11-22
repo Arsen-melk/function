4a0b11627850351490cad0037b55fc4313c1c7e7
arsen-melk <arsenmelk001@gmail.com>
2022-11-22 15:01:01 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,36 @@
+
+console.log(`"""""""""""""""""""""""`); 
+ function main(a){
+
+
+function qar(){
+  console.log(a**2);
+}
+qar();
+
+}
+main(12);
+main(5);
+main(10);
+
+console.log(`"""""""""""""""""""""""`); 
+
+function main(a) {
+
+   function num2(){
+     
+      console.log(Math.pow(a,2));
+      
+   }
+   
+   num2(); 
+
+}
+main(3);
+main(5);
+main(10);
+main(13);
+main(21);
+
+console.log(`"""""""""""""""""""""""`); 
+
